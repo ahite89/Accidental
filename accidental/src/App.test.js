d40b5adf75104b82f73dc99e57e34414aa389727
ahite <ahite89@gmail.com>
2023-04-23 23:33:43 -0400

    delete frontend project

--- accidental/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('displays app name', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Accidental/i);
-  expect(linkElement).toBeInTheDocument();
-});
