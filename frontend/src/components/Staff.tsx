cdd0e285ad8aec132e29bea3e8eb50e54917935a
ahite <ahite89@gmail.com>
2023-04-26 03:45:34 -0400

    add comments with next steps

--- frontend/src/components/Staff.tsx
+++ frontend/src/components/Staff.tsx
@@ -4,14 +4,26 @@ import abcjs from "abcjs";
 export default function Staff() {
     useEffect(() => {
         const abcjsInit = async () => {
-            //const abcjs = await import("abcjs");
             const note = 'b4'
-            abcjs.renderAbc("staff", `X:1\nK:F\nEEA2|bbA2G${note}eFGA|\nAAABBDD|gA4`)
+            abcjs.renderAbc("staff", `X:1\nK:F\naaz2BBb''|cDd_d,,f^|\nabBe2g^4`)
         };
         abcjsInit();
     }, []);
 
     return (
-        <div id="staff"></div>
+        <div style={{border: '1px solid gray'}} id="staff"></div>
     );
 }
+
+// Function: while generating, feed array of note objects into rendering loop
+//
+// create an array of note objects
+// create a starting string with the clef, key, and some hidden notes (do so in useEffect)
+// in click event handler, call a function that iterates over the array
+// while looping, continuously append notes to the end of the string, re-rendering after each addition
+// use string interpolation to add note name, duration, etc. to the string
+// use setTimeout or setInterval to handle how quickly the rendering occurs
+// put in a barline after four beats 
+// put in a newline regularly
+// if (first beat of current bar note == last beat of previous bar note) => use tie
+// make the newest note a different color?
