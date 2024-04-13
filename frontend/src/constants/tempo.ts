76a7f578dc6f494883bb8b6faa93ee6cfcf36f1d
ahite89 <ahite89@gmail.com>
2024-04-13 17:30:10 -0400

    factor in selected tempo to randomizer

--- frontend/src/constants/tempo.ts
+++ frontend/src/constants/tempo.ts
@@ -1,4 +1,4 @@
 export const MIN_TEMPO = 60;
 export const MAX_TEMPO = 180;
 export const TEMPO_INTERVAL = 4;
-export const DEFAULT_TEMPO = 100;
+export const DEFAULT_TEMPO = 120;
--- frontend/src/services/noteRandomizer.ts
+++ frontend/src/services/noteRandomizer.ts
@@ -11,17 +11,20 @@ export const getRandomizedNote = (notationObj: NotationData): NoteProps => {
     const randomDurationIndex = Math.floor(Math.random() * selectedDurations.length);
     const randomDuration = selectedDurations[randomDurationIndex];
 
-    debugger
-
     // Get random pitch
     const validNotesMultiArray = notationObj.validNotesForRandomizing;
     const randomPitchIndex = Math.floor(Math.random() * validNotesMultiArray.length);
     const randomNameAndPitch = validNotesMultiArray[randomPitchIndex];
+
+    // Get time between notes based on selected tempo
+    // 1 Minute or 60,000 milliseconds for quarter note
+    const selectedTempo = notationObj.randomizerParams.tempoSelection;
+    const eighthNoteDurationInMilliseconds = 30000 / selectedTempo
     
     return { 
         abcName: randomNameAndPitch[0] as string,
         pitchNumber: +randomNameAndPitch[1],
         durationProps: randomDuration,
-        timeBetweenNotes: 300 * randomDuration.audioDuration
+        timeBetweenNotes: eighthNoteDurationInMilliseconds * randomDuration.audioDuration
     };
 };
