ddc8d1eee1109526923dfdcb98095f3708c5ef27
ahite89 <ahite89@gmail.com>
2024-06-29 00:56:50 -0400

    add opacity to primary button on hover

--- frontend/src/components/parameters/Button.tsx
+++ frontend/src/components/parameters/Button.tsx
@@ -13,7 +13,7 @@ export default function Button({
 }: ButtonProps) {
 
     const classes = className('px-6 py-3', {
-        'border border-cyan-500 bg-cyan-500 text-white': primary,
+        'border border-cyan-500 bg-cyan-500 text-white hover:opacity-75': primary,
         'border border-slate-400 bg-slate-400 text-white hover:opacity-100': disabled,
         'rounded-full': rounded,
         'bg-transparent border border-white text-white hover:bg-cyan-500': outline
